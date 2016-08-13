8b9916bc1dab33c3c496b1ed7f89da785c1ecc7d
Yourtion <yourtion@gmail.com>
2016-08-13 17:25:17 +0000

    bower_components

---
+++ gulpfile.js
@@ -0,0 +1,115 @@
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const gulpif = require('gulp-if');
+const streamify = require('gulp-streamify');
+const autoprefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-cssmin');
+const less = require('gulp-less');
+const concat = require('gulp-concat');
+const plumber = require('gulp-plumber');
+const source = require('vinyl-source-stream');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const uglify = require('gulp-uglify');
+
+const production = process.env.NODE_ENV === 'production';
+
+const dependencies = [
+  'alt',
+  'react',
+  'react-router',
+  'underscore'
+];
+
+/*
+ |--------------------------------------------------------------------------
+ | Combine all JS libraries into a single file for fewer HTTP requests.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('vendor', function() {
+  return gulp.src([
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/bootstrap/dist/js/bootstrap.js',
+    'bower_components/magnific-popup/dist/jquery.magnific-popup.js',
+    'bower_components/toastr/toastr.js'
+  ]).pipe(concat('vendor.js'))
+    .pipe(gulpif(production, uglify({ mangle: false })))
+    .pipe(gulp.dest('public/js'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile third-party dependencies separately for faster performance.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-vendor', function() {
+  return browserify()
+    .require(dependencies)
+    .bundle()
+    .pipe(source('vendor.bundle.js'))
+    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+    .pipe(gulp.dest('public/js'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile only project files, excluding all third-party dependencies.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify', ['browserify-vendor'], function() {
+  return browserify('app/main.js')
+    .external(dependencies)
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+    .pipe(gulp.dest('public/js'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Same as browserify task, but will also watch for changes and re-compile.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-watch', ['browserify-vendor'], function() {
+  const bundler = watchify(browserify('app/main.js', watchify.args));
+  bundler.external(dependencies);
+  bundler.transform(babelify);
+  bundler.on('update', rebundle);
+  return rebundle();
+
+  function rebundle() {
+    var start = Date.now();
+    return bundler.bundle()
+      .on('error', function(err) {
+        gutil.log(gutil.colors.red(err.toString()));
+      })
+      .on('end', function() {
+        gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
+      })
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('public/js/'));
+  }
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile LESS stylesheets.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('styles', function() {
+  return gulp.src('app/stylesheets/main.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulpif(production, cssmin()))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/stylesheets/**/*.less', ['styles']);
+});
+
+gulp.task('default', ['styles', 'vendor', 'browserify-watch', 'watch']);
+gulp.task('build', ['styles', 'vendor', 'browserify']);
