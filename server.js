f659a4b9c5d9d919cd6889f77b46d7f03585f875
Yourtion <yourtion@gmail.com>
2016-08-13 14:17:50 +0000

    base server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
