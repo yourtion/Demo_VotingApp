7c28047d9be28e2c2206c537f64ca707009c5fef
Yourtion <yourtion@gmail.com>
2016-08-13 17:58:38 +0000

    basic server

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+const swig  = require('swig');
+const React = require('react');
+const Router = require('react-router');
+const routes = require('./app/routes');
+
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -12,6 +17,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res) {
+  Router.run(routes, req.path, function(Handler) {
+    var html = React.renderToString(React.createElement(Handler));
+    var page = swig.renderFile('views/index.html', { html: html });
+    res.send(page);
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
