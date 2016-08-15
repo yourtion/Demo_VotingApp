7ba56df61bdd3abda724be40b7d8b09addf41c6b
Yourtion <yourtion@gmail.com>
2016-08-15 03:38:52 +0000

    NavBar

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Footer'
+import Navbar from './Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar history={this.props.history} />
         {this.props.children}
         <Footer />
       </div>
