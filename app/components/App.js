781bebc899b2ed065eaeaf7335a9a7ce0dbf867f
Yourtion <yourtion@gmail.com>
2016-08-15 03:27:42 +0000

    Footer

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './Footer'
 
 class App extends React.Component {
   render() {
     return (
       <div>
         {this.props.children}
+        <Footer />
       </div>
     );
   }
