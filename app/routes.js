7917a61ff12ea6d43975476334418a81f24ddd9a
Yourtion <yourtion@gmail.com>
2016-08-29 05:51:44 +0000

    CharacterList

--- app/routes.js
+++ app/routes.js
@@ -4,13 +4,19 @@ import React from 'react';
 import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
-import AddCharacter from './components/AddCharacter';
 import Character from './components/Character';
+import CharacterList from './components/CharacterList';
+import AddCharacter from './components/AddCharacter';
 
 export default (
   <Route component={App}>
     <Route path='/' component={Home} />
     <Route path='/add' component={AddCharacter} />
     <Route path='/characters/:id' component={Character} />
+    <Route path=':category' component={CharacterList}>
+      <Route path=':race' component={CharacterList}>
+        <Route path=':bloodline' component={CharacterList} />
+      </Route>
+    </Route>
   </Route>
 );
