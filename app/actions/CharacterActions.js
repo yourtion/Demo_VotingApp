8eeb225fded5021af113dc97e0acc1898e6990d7
Yourtion <yourtion@gmail.com>
2016-08-29 05:47:41 +0000

    Character profile

---
+++ app/actions/CharacterActions.js
@@ -0,0 +1,38 @@
+import alt from '../alt';
+
+class CharacterActions {
+  constructor() {
+    this.generateActions(
+      'reportSuccess',
+      'reportFail',
+      'getCharacterSuccess',
+      'getCharacterFail'
+    );
+  }
+
+  getCharacter(characterId) {
+    $.ajax({ url: '/api/characters/' + characterId })
+      .done((data) => {
+        this.getCharacterSuccess(data);
+      })
+      .fail((jqXhr) => {
+        this.getCharacterFail(jqXhr);
+      });
+  }
+
+  report(characterId) {
+    $.ajax({
+      type: 'POST',
+      url: '/api/report',
+      data: { characterId: characterId }
+    })
+      .done(() => {
+        this.reportSuccess();
+      })
+      .fail((jqXhr) => {
+        this.reportFail(jqXhr);
+      });
+  }
+}
+
+export default alt.createActions(CharacterActions);
--- app/routes.js
+++ app/routes.js
@@ -5,10 +5,12 @@ import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import AddCharacter from './components/AddCharacter';
+import Character from './components/Character';
 
 export default (
   <Route component={App}>
     <Route path='/' component={Home} />
     <Route path='/add' component={AddCharacter} />
+    <Route path='/characters/:id' component={Character} />
   </Route>
 );
