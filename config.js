cbdc7ffa6d4230d9cb0cb94d50430729c95f5f65
郭宇翔 <yourtion@gmail.com>
2017-07-05 08:18:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: process.env.MONGO_URI || process.env.MONGODB_CONNECTION || 'localhost/nef',
+  database: process.env.MONGODB_URI || process.env.MONGODB_CONNECTION || 'localhost/nef',
 };
